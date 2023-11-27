8bc115c2225576384546e14a81b63f7e22f9093e
Dome1992 <forlanodomenico92@mail.com>
2023-11-27 18:27:08 +0100

    function parole palindrome

--- Function Palindrome/script.js
+++ Function Palindrome/script.js
@@ -3,19 +3,38 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 
 // chiedere all'utente di inserire una parola
+
 const parolaUtente = prompt("Inserisci una parola:");
 console.log(parolaUtente);
 
+
 // Chiamare la funzione per invertire la parola e stampare il risultato
+
 const parolaInvertita = invertiParola(parolaUtente);
 console.log("Parola invertita:", parolaInvertita);
 
 
 // Funzione per invertire una parola
+
 function invertiParola(parola) {
     const parolaInvertita = parola.split('').reverse().join('');
-    console.log("Parola invertita nella funzione:", parolaInvertita);
+    
     return parolaInvertita;
     
 
 }   
+
+
+// if ed else per capire se parola è palindroma o meno 
+    
+let risultato;
+if (parolaInvertita === parolaUtente) {
+    risultato = "La parola è palindroma"
+    alert("La parola è palindroma");
+    console.log(risultato);
+
+} else {
+    risultato = "La parola non è palindroma"
+    alert("La parola non è palindroma");
+    console.log(risultato);
+}
