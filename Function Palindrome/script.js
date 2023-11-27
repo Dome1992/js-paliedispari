c16b6babb4726b507eb83304fa45f3a7123482f9
Dome1992 <forlanodomenico92@mail.com>
2023-11-27 16:29:52 +0100

    feat. setup iniziale

---
+++ Function Palindrome/script.js
@@ -0,0 +1,7 @@
+/* Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+
+// chiedere all'utente di inserire una parola
+const parolaUtente = prompt("Inserisci una parola:");
+console.log(parolaUtente);
