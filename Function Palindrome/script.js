7dc9ae9e11f94daafefc31bfed652769cf7f1c29
Dome1992 <forlanodomenico92@mail.com>
2023-11-27 17:25:32 +0100

    feat. function parola palindroma incompleto

--- Function Palindrome/script.js
+++ Function Palindrome/script.js
@@ -5,3 +5,10 @@ Creare una funzione per capire se la parola inserita è palindroma */
 // chiedere all'utente di inserire una parola
 const parolaUtente = prompt("Inserisci una parola:");
 console.log(parolaUtente);
+
+// Chiamare la funzione e stampare il risultato
+const risultato = palindroma(parolaUtente);
+console.log(risultato);
+
+
+
